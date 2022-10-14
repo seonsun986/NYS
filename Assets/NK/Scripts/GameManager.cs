45428cb9af1261def15b49f2e32ef69c04d82939
kimnakyung <35446851+dd6679@users.noreply.github.com>
2022-10-14 20:36:04 +0900

    update mute
    선생님 UI 음소거 기능 추가

---
+++ Assets/NK/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    //public List<PhotonView> players = new List<PhotonView> ();
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // 현재 방에 있는 아이들 (포톤뷰로 바꿀 예정)
+    //public List<PhotonView> children = new List<PhotonView> ();
+    public List<GameObject> children = new List<GameObject>();
+    public void AddPlayer(/*PhotonView pv*/ GameObject go)
+    {
+        if(go.CompareTag("Child"))
+            children.Add(go);
+    }
+}
