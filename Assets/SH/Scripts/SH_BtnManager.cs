26a42a5de389eb4670427a771155a7eeaa814e1f
seonsun986 <108320882+seonsun986@users.noreply.github.com>
2022-10-17 00:04:36 +0900

    툴 수정
    .

--- Assets/SH/Scripts/SH_BtnManager.cs
+++ Assets/SH/Scripts/SH_BtnManager.cs
@@ -3,6 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+public class TextInfo
+{
+    public Text inputs { get; set; }
+    public int txtDropdown { get; set; }
+    public int txtSize { get; set; }
+}
+
 public class SH_BtnManager : MonoBehaviour
 {
     public Image sceneBG;
@@ -13,6 +20,16 @@ public class SH_BtnManager : MonoBehaviour
     Animation objectAnim;
     bool sceneBG_view;
     bool objectBG_view;
+
+    public GameObject inputField;       // inputField 프리팹
+    public List<TextInfo> inputFields = new List<TextInfo>();
+    // 현재 선택되어있는 드롭다운과 텍스트 사이즈
+    public Dropdown txtDropdown;
+    public Text txtSize;
+
+    // 씬 추가하기
+    public GameObject voidScene;
+    public GameObject rawImage;
 
     void Start()
     {
@@ -29,19 +46,25 @@ public class SH_BtnManager : MonoBehaviour
     #region 에디터 배경 나오기 버튼 함수들
     public void MoveSceneBG()
     {
-        // sceneBG가 보이지 않는다면
+        // sceneBG가 보이지 않는다면       
         if(sceneBG_view == false)
         {
-            sceneAnim.Play("SceneBGShowAnim");
+            sceneAnim.Play("SceneBGShowAnim");            
             SceneBtn.rotation = new Quaternion(0, 0, -180,0);       // 버튼 돌려주기
             sceneBG_view = true;
+            // 이때 objectBG가 보인다면
+            if(objectBG_view == true)
+            {
+                objectAnim.Play("ObjectBGShowOffAnim");
+                ObjectBtn.rotation = new Quaternion(0, 0, -180, 0);
+                objectBG_view = false;
+            }
         }
         // 그렇지 않다면
         else
         {
             sceneAnim.Play("SceneBGShowOffAnim");
             SceneBtn.rotation = new Quaternion(0, 0, 0, 0);
-
             sceneBG_view = false;
         }
     }
@@ -54,6 +77,13 @@ public class SH_BtnManager : MonoBehaviour
             objectAnim.Play("ObjectBGShowAnim");
             ObjectBtn.rotation = new Quaternion(0, 0, 0, 0);       // 버튼 돌려주기
             objectBG_view = true;
+            // 이때 SceneBG가 보인다면
+            if(sceneBG_view == true)
+            {
+                sceneAnim.Play("SceneBGShowOffAnim");
+                SceneBtn.rotation = new Quaternion(0, 0, 0, 0);
+                sceneBG_view = false;
+            }
         }
 
         //그렇지 않다면
@@ -65,5 +95,55 @@ public class SH_BtnManager : MonoBehaviour
         }
     }
     #endregion
+
+
+    // 텍스트 추가 함수
+    // 텍스트를 추가할 때 처음엔 기본 값으로 세팅한다
+    // 드롭다운이 변형될 때마다 폰트를 변경한다
+    // 텍스트 사이즈를 변경할 때마다 폰트 크기를 변경한다
+    // 변경할 때마다의 값을 각자만의 클래스에 저장해놓는다.
+    public void AddText()
+    {
+        GameObject text = Instantiate(inputField);
+        // 선택되어있는 dropdown과 textSize값에 따라서 글자 크기를 바꾸기 위함
+        inputFields.Add(new TextInfo
+        {
+            inputs = text.GetComponent<Text>(),
+            txtDropdown = txtDropdown.value,
+            txtSize = int.Parse(txtSize.text),
+        }) ;        
+        text.transform.SetParent(GameObject.Find("Canvas").transform);
+        text.transform.localPosition = new Vector3(0, 0, 0);
+
+    }
+
+    public void PlusSize()
+    {
+        int size = int.Parse( txtSize.text);
+        size++;
+        txtSize.text = size.ToString();
+    }
+
+    public void MinusSize()
+    {
+        int size = int.Parse(txtSize.text);
+        size--;
+        txtSize.text = size.ToString();
+    }
+
+
+    // 씬 추가하기 함수
+    // Save를 누르거나, 씬을 추가하는 순간 해당 부분을 캡쳐한다.
+    int i = 0;
+    public void AddScene()
+    {
+        GameObject scene = Instantiate(voidScene);
+        GameObject raw = Instantiate(rawImage);
+        raw.transform.SetParent(sceneBG.transform);
+        scene.transform.SetParent(GameObject.Find("CanvasForScene").transform);
+        scene.transform.position = new Vector3(0, 0.5f, 0);
+        i++;
+        print(i);
+    }
 
 }
---
+++ Assets/SH/Scripts/SH_TextManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class SH_TextManager : MonoBehaviour
+{    
+    public Dropdown txtDropdown;
+    public Text txtSize;
+    // 현재 dropdown의 value값에 따라 글씨체를 바꾸고 싶다
+    // 각 텍스트마다 dropdown value값과 글씨사이즈를 저장해둘것이 필요하다
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
