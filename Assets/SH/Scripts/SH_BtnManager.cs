4b756398893556f290ef5ba681642c83dab71803
seonsun986 <108320882+seonsun986@users.noreply.github.com>
2022-10-20 18:38:49 +0900

    오브젝트 나타나기 완료

--- Assets/SH/Scripts/SH_BtnManager.cs
+++ Assets/SH/Scripts/SH_BtnManager.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.IO;
+
 
 
 // InputField 필요한 정보들
@@ -32,11 +35,28 @@ public class SH_BtnManager : MonoBehaviour
     // 씬 추가하기
     public GameObject voidScene;
     public GameObject rawImage;
+    public Camera sceneCam;         // 씬 카메라
+    public List<RawImage> rawImages = new List<RawImage>();
+    string path;                    // 캡쳐 정보 저장 경로
+    private int captureWidth;
+    private int captureHeight;
+        
+    // Object 추가를 위한 List
+    public GameObject[] obj;
+
+    // RawImage에 따른 씬 카메라 위치 리스트
+    public List<Vector3> camPos = new List<Vector3>();
 
     void Start()
     {
         sceneAnim = sceneBG.GetComponent<Animation>();
         objectAnim = objectBG.GetComponent<Animation>();
+        path = Application.dataPath + "/Capture/";
+        captureWidth = Screen.width;
+        captureHeight = Screen.height;
+        sceneCamPos = sceneCam.transform.position;
+        // 처음 포지션을 추가해준다
+        camPos.Add(new Vector3(0, 0.46f, -8.2f));
     }
 
     void Update()
@@ -137,17 +157,79 @@ public class SH_BtnManager : MonoBehaviour
 
 
     // 씬 추가하기 함수
-    // Save를 누르거나, 씬을 추가하는 순간 해당 부분을 캡쳐한다.
+    // rawImage를 리스트에 담는다
+    // Save를 누르거나, 씬을 추가하는 순간 해당 씬을 캡쳐한다.
+    // 캡쳐한 후에 rawImage에 해당 이미지를 담고
+    // 새로운 rawImage를 추가한다
+    // 씬 카메라를 아래로 내린다
+    Vector3 sceneCamPos;
+    Vector3 sceneCamAddPos;
+    string fileName;            // 파일 저장 이름
     int i = 0;
     public void AddScene()
     {
-        GameObject scene = Instantiate(voidScene);
+        #region 캡쳐하기
+        // 파일이 없다면
+        DirectoryInfo dir = new DirectoryInfo(path);
+        if (!dir.Exists)
+        {
+            Directory.CreateDirectory(path);
+        }
+        
+        // 캡쳐파일 이름 정하기
+        fileName = path + "_" + i + ".png";
+
+        // 캡쳐하기 
+        RenderTexture rt = new RenderTexture(captureWidth, captureHeight, 24);
+        sceneCam.targetTexture = rt;
+        Texture2D screenShot = new Texture2D(captureWidth, captureHeight, TextureFormat.RGB24, false);
+        Rect rec = new Rect(0, 0, screenShot.width, screenShot.height);
+        sceneCam.Render();
+        RenderTexture.active = rt;
+        screenShot.ReadPixels(new Rect(0, 0, captureWidth, captureHeight), 0, 0);
+        screenShot.Apply();
+
+        byte[] bytes = screenShot.EncodeToPNG();
+        File.WriteAllBytes(fileName, bytes);
+        #endregion
+
+
+        // 캡쳐파일 RawImage에 넣기
+        byte[] textureBytes = File.ReadAllBytes(fileName);
+        if(textureBytes.Length>0)
+        {
+            Texture2D loadedTexture = new Texture2D(0, 0);
+            loadedTexture.LoadImage(textureBytes);
+            rawImages[i].GetComponent<RawImage>().texture = loadedTexture;
+        }
+         
+        // 새로운 Rawimage 추가
         GameObject raw = Instantiate(rawImage);
-        raw.transform.SetParent(sceneBG.transform);
-        scene.transform.SetParent(GameObject.Find("CanvasForScene").transform);
-        scene.transform.position = new Vector3(0, 0.5f, 0);
+        rawImages.Add(raw.GetComponent<RawImage>());
+
+        // 카메라 내리기
+        // Vector3(0,0.460000008,-8.18999958) 위치 저장 y값으로 -10만큼!
+        camPos.Add( camPos[i] + new Vector3(0, -10, 0));
+        sceneCam.transform.position = Vector3.Lerp(camPos[i], camPos[i + 1],0.5f);
         i++;
-        print(i);
+        
+    }
+
+    // Object 생성 함수
+    public void InstantiateObj()
+    {
+        GameObject clickBtn = EventSystem.current.currentSelectedGameObject;
+        string clickText = clickBtn.name.Substring(0,clickBtn.name.Length - 3);
+        // 이름에 해당하는 Object를 Instantiate 한다
+        for(int i =0;i<obj.Length;i++)
+        {
+            if(obj[i].name.Contains(clickText))
+            {
+                GameObject createObj = Instantiate(obj[i]);
+                createObj.transform.position = new Vector3(0, 0, 0);
+                break;
+            }
+        }
     }
 
 }
