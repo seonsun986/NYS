b9069154c2f966232efea23bdc3cb377a40f3c7d
kimnakyung <35446851+dd6679@users.noreply.github.com>
2022-10-22 14:06:29 +0900

    update editorscene2
    뒤로가기 기능 추가

--- Assets/NK/Scripts/NK_ViewMore.cs
+++ Assets/NK/Scripts/NK_ViewMore.cs
@@ -7,6 +7,9 @@ using UnityEngine.EventSystems;
 public class NK_ViewMore : MonoBehaviour
 {
     public GameObject moreSection;
+    public GameObject moreText;
+    public GameObject addBtn;
+    public GameObject backBtn;
     public List<GameObject> sections = new List<GameObject> ();
 
     // Start is called before the first frame update
@@ -21,18 +24,54 @@ public class NK_ViewMore : MonoBehaviour
         
     }
 
-    public void ViewMore()
+    #region ClickViewMore // 더보기 버튼 클릭
+    public void ClickViewMore()
     {
+        // 방금 클릭한 더보기 버튼을 가져오기
+        addBtn = EventSystem.current.currentSelectedGameObject;
+        // 더보기 버튼의 부모를 가져오기
         moreSection = EventSystem.current.currentSelectedGameObject.transform.parent.gameObject;
-        RectTransform rectTransform = moreSection.GetComponent<RectTransform> ();
-        rectTransform.SetHeight(500);
+        RectTransform sectionTransform = moreSection.GetComponent<RectTransform> ();
+        sectionTransform.SetHeight(500);
+        // 텍스트 가져오기
+        moreText = moreSection.transform.GetChild(0).gameObject;
+        RectTransform textTreansform = moreText.GetComponent<RectTransform> ();
+        textTreansform.SetPosX(70);
+
+        // 두번째 줄부터의 오브젝트 나타내기
+        GameObject.Find(moreSection.name).transform.Find("More").gameObject.SetActive(true);
 
         foreach (var section in sections)
         {
+            // 만약 선택한 section이 아니라면 비활성화 처리
             if(section.gameObject != moreSection)
             {
                 section.SetActive(false);
             }
         }
+        addBtn.SetActive(false);
+        backBtn.SetActive(true);
     }
+    #endregion
+
+    #region ClickBack // 뒤로가기 버튼 클릭
+    public void ClickBack()
+    {
+        RectTransform sectionTransform = moreSection.GetComponent<RectTransform>();
+        sectionTransform.SetHeight(100);
+
+        RectTransform textTreansform = moreText.GetComponent<RectTransform>();
+        textTreansform.SetPosX(30);
+
+        // 두번째 줄부터의 오브젝트 숨기기
+        GameObject.Find(moreSection.name).transform.Find("More").gameObject.SetActive(false);
+
+        foreach (var section in sections)
+        {
+            section.SetActive(true);
+        }
+        addBtn.SetActive(true);
+        backBtn.SetActive(false);
+    }
+    #endregion
 }
