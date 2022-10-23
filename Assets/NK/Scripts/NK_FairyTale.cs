79a28a8901e43bf6312798dd72f527ba0f04cdee
kimnakyung <35446851+dd6679@users.noreply.github.com>
2022-10-23 18:33:10 +0900

    update fairytale

--- Assets/NK/Scripts/NK_FairyTale.cs
+++ Assets/NK/Scripts/NK_FairyTale.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class NK_FairyTale : MonoBehaviour
 {
+    public GameObject fairyTaleUI;
+    public GameObject fairyTaleObject;
+    public Transform book;
+    public List<Transform> objs;
     // Start is called before the first frame update
     void Start()
     {
-        
+        AddObjectList(fairyTaleUI);
+
+        AddObjectList(fairyTaleObject);
+    }
+
+    private void AddObjectList(GameObject fairyTaleContent)
+    {
+        foreach (Transform child in fairyTaleContent.GetComponentsInChildren<Transform>())
+        {
+            if (child.name == fairyTaleContent.name)
+                continue;
+            child.localScale = new Vector3(0, 0, 0);
+            if (child.GetComponent<Renderer>() != null)
+            {
+                Renderer ren = child.GetComponent<Renderer>();
+                Color color = ren.material.color;
+                print(color);
+                color.a = 0;
+                ren.material.color = color;
+            }
+            objs.Add(child);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        for (int i = 0; i < objs.Count; i++)
+        {
+            if (objs[i].gameObject.activeSelf == true)
+            {
+                if (objs[i].transform.localScale.x < 1f)
+                {
+                    objs[i].transform.localScale += new Vector3(0.1f, 0.1f, 0.1f);
+                }
+            }
+
+        }
     }
 }
