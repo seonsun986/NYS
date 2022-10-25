8e21c297031b479e5c5016577438c7dad0957781
seonsun986 <108320882+seonsun986@users.noreply.github.com>
2022-10-25 15:54:40 +0900

    스카이박스 에셋 다운

--- Assets/SH/Scripts/SH_EditorManager.cs
+++ Assets/SH/Scripts/SH_EditorManager.cs
@@ -14,6 +14,12 @@ public class SH_EditorManager : MonoBehaviour
     public Font[] fonts;
     #endregion
 
+    // 생성된 오브젝트 애니메이션 담아놓는다
+    [Header("오브젝트 애니메이션")]
+    public List<Animation> anim = new List<Animation>();
+    // 딕셔너리 선언해서 Walk -> 걷기
+    public Dictionary<string, string> animName;
+    
     // 선택한 버튼에 따라 Instantiate되는 친구들이 달라진다
     private void Awake()
     {
@@ -21,7 +27,13 @@ public class SH_EditorManager : MonoBehaviour
     }
     void Start()
     {
-        
+        animName = new Dictionary<string, string>
+        {
+            { "Walk", "걷기"},
+            { "Run", "뛰기"},
+            {"Eat", "먹기" },
+            { "Jump", "점프" },
+        };
     }
 
     void Update()
