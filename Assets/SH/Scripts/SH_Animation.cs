c58ad634c6a9ace9a367680a59ccc68609c7da7d
seonsun986 <108320882+seonsun986@users.noreply.github.com>
2022-11-03 17:33:30 +0900

    .
    .

---
+++ Assets/SH/Scripts/SH_Animation.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SH_Animation : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        // ∞ı¿œ ∂ß
+        if(gameObject.name.Contains("Bear"))
+        {
+
+        }
+    }
+}
