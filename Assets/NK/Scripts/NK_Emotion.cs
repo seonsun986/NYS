8d66c1c564b423e3f82bd4bf39f657e0d8b903bb
kimnakyung <35446851+dd6679@users.noreply.github.com>
2022-11-06 14:20:37 +0900

    update emotion

--- Assets/NK/Scripts/NK_Emotion.cs
+++ Assets/NK/Scripts/NK_Emotion.cs
@@ -18,7 +18,11 @@ public class NK_Emotion : MonoBehaviourPun
     public Transform emotionUI;
     public GameObject clickUser;
     public Emotion emotion = Emotion.NoSelection;
+    public List<GameObject> emojis = new List<GameObject>();
+    public Animator anim;
+    public float emotionTime = 2f;
     bool mouseClick;
+    float currentTime;
     // Start is called before the first frame update
     void Start()
     {
@@ -40,25 +44,50 @@ public class NK_Emotion : MonoBehaviourPun
 
         if(NK_EmotionUI.emotion != Emotion.NoSelection)
         {
-            clickUser.transform.forward = - gameObject.transform.forward;
+            //clickUser.transform.forward = - gameObject.transform.forward;
+            emotion = NK_EmotionUI.emotion;
+            ShowEmotion();
+            currentTime += Time.deltaTime;
+        }
+
+        if(emotionTime < currentTime)
+        {
+            InitializationEmotion();
+            currentTime = 0;
         }
     }
 
     private void ClickUser()
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
+        emotionUI.gameObject.SetActive(true);
+        emotionUI.transform.position = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+        mouseClick = false;
+        /*        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
 
-        if(Physics.Raycast(ray, out hit))
-        {
-            clickUser = hit.transform.gameObject;
+                if(Physics.Raycast(ray, out hit))
+                {
+                    clickUser = hit.transform.gameObject;
 
-            if (hit.transform.gameObject.tag == "Child")
-            {
-                emotionUI.gameObject.SetActive(true);
-                emotionUI.transform.position = gameObject.transform.position;
-                mouseClick = false;
-            }
-        }
+                    if (hit.transform.gameObject.tag == "Child")
+                    {
+                        emotionUI.gameObject.SetActive(true);
+                        emotionUI.transform.position = gameObject.transform.position;
+                        mouseClick = false;
+                    }
+                }*/
+    }
+
+    private void ShowEmotion()
+    {
+        emojis[(int)emotion - 1].SetActive(true);
+        anim.SetTrigger(emotion.ToString());
+    }
+
+    private void InitializationEmotion()
+    {
+        emojis[(int)emotion - 1].SetActive(false);
+        anim.SetTrigger("Idle");
+        emotion = Emotion.NoSelection;
     }
 }
