f50815b159711696fa7b5fcb31927b6db7a59215
seonsun986 <108320882+seonsun986@users.noreply.github.com>
2022-11-11 09:52:46 +0900

    .
    .

--- Assets/SH/Scripts/SH_Page9.cs
+++ Assets/SH/Scripts/SH_Page9.cs
@@ -5,14 +5,202 @@ using UnityEngine;
 public class SH_Page9 : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Animator mushroom;
+    public Animator egg;
+    public Animator spinach;
+    public Animator rice;
+    public Animator onion;
+    public Animator potato;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    RaycastHit hitInfo;
     void Update()
     {
-        
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(ray ,out hitInfo))
+        {
+            if(hitInfo.transform.name == "MushroomBtn")
+            {
+                if(mushroom.enabled == false)
+                {
+                    mushroom.enabled = true;
+                    egg.Rebind();
+                    egg.enabled = false;
+                    spinach.Rebind();
+                    spinach.enabled = false;
+                    rice.Rebind();
+                    rice.enabled = false;
+                    onion.Rebind();
+                    onion.enabled = false;
+                    potato.Rebind();
+                    potato.enabled = false;
+                }
+
+                if(Input.GetMouseButtonDown(0))
+                {
+                    SH_ChildrenFairyManager.Instance.PassTrue();
+                    SH_ChildrenFairyManager.Instance.FillEmptyBox();
+                }
+            }
+
+
+            else if (hitInfo.transform.name == "EggBtn")
+            {
+                if (egg.enabled == false)
+                {
+                    egg.enabled = true;
+                    mushroom.Rebind();
+                    mushroom.enabled = false;
+                    spinach.Rebind();
+                    spinach.enabled = false;
+                    rice.Rebind();
+                    rice.enabled = false;
+                    onion.Rebind();
+                    onion.enabled = false;
+                    potato.Rebind();
+                    potato.enabled = false;
+                }
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    SH_ChildrenFairyManager.Instance.PassTrue();
+                    SH_ChildrenFairyManager.Instance.FillEmptyBox();
+                }
+            }
+
+
+            else if (hitInfo.transform.name == "SpinachBtn")
+            {
+                if (spinach.enabled == false)
+                {
+                    spinach.enabled = true;
+                    mushroom.Rebind();
+                    mushroom.enabled = false;
+                    egg.Rebind();
+                    egg.enabled = false;
+                    rice.Rebind();
+                    rice.enabled = false;
+                    onion.Rebind();
+                    onion.enabled = false;
+                    potato.Rebind();
+                    potato.enabled = false;
+                }
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    SH_ChildrenFairyManager.Instance.PassTrue();
+                    SH_ChildrenFairyManager.Instance.FillEmptyBox();
+                }
+            }
+
+            else if (hitInfo.transform.name == "RiceBtn")
+            {
+                if (rice.enabled == false)
+                {
+                    rice.enabled = true;
+                    mushroom.Rebind();
+                    mushroom.enabled = false;
+                    spinach.Rebind();
+                    spinach.enabled = false;
+                    egg.Rebind();
+                    egg.enabled = false;
+                    onion.Rebind();
+                    onion.enabled = false;
+                    potato.Rebind();
+                    potato.enabled = false;
+                }
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    SH_ChildrenFairyManager.Instance.PassTrue();
+                    SH_ChildrenFairyManager.Instance.FillEmptyBox();
+                }
+            }
+
+            else if (hitInfo.transform.name == "OnionBtn")
+            {
+                if (onion.enabled == false)
+                {
+                    onion.enabled = true;
+                    mushroom.Rebind();
+                    mushroom.enabled = false;
+                    spinach.Rebind();
+                    spinach.enabled = false;
+                    egg.Rebind();
+                    egg.enabled = false;
+                    potato.Rebind();
+                    potato.enabled = false;
+                    rice.Rebind();
+                    rice.enabled = false;
+                }
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    SH_ChildrenFairyManager.Instance.PassTrue();
+                    SH_ChildrenFairyManager.Instance.FillEmptyBox();
+                }
+            }
+
+            else if (hitInfo.transform.name == "PotatoBtn")
+            {
+                if (potato.enabled == false)
+                {
+                    potato.enabled = true;
+                    mushroom.Rebind();
+                    mushroom.enabled = false;
+                    spinach.Rebind();
+                    spinach.enabled = false;
+                    egg.Rebind();
+                    egg.enabled = false;
+                    onion.Rebind();
+                    onion.enabled = false;
+                    rice.Rebind();
+                    rice.enabled = false;
+                }
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    SH_ChildrenFairyManager.Instance.PassTrue();
+                    SH_ChildrenFairyManager.Instance.FillEmptyBox();
+                }
+            }
+
+            else
+            {
+                mushroom.Rebind();
+                mushroom.enabled = false;
+                spinach.Rebind();
+                spinach.enabled = false;
+                egg.Rebind();
+                egg.enabled = false;
+                onion.Rebind();
+                onion.enabled = false;
+                rice.Rebind();
+                rice.enabled = false;
+                potato.Rebind();
+                potato.enabled = false;
+
+            }
+
+        }
+
+        else
+        {
+            mushroom.Rebind();
+            mushroom.enabled = false;
+            spinach.Rebind();
+            spinach.enabled = false;
+            egg.Rebind();
+            egg.enabled = false;
+            onion.Rebind();
+            onion.enabled = false;
+            rice.Rebind();
+            rice.enabled = false;
+            potato.Rebind();
+            potato.enabled = false;
+        }
     }
 }
