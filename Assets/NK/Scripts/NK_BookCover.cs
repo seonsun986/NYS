d3dd269f4dec6af9d34c44976909e82a871d4896
kimnakyung <35446851+dd6679@users.noreply.github.com>
2022-11-14 06:21:25 +0900

    update bookcover

--- Assets/NK/Scripts/NK_BookCover.cs
+++ Assets/NK/Scripts/NK_BookCover.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
@@ -34,9 +36,14 @@ public class NK_BookCover : MonoBehaviour
     private void OnEnable()
     {
         taleInfo = new TaleInfo();
-        // 색, 폰트, 크기, 팔레트 켜짐 초기화
         palette.SetActive(false);
         bgPalette.SetActive(false);
+        Initialization();
+    }
+
+    public void Initialization()
+    {
+        // 색, 폰트, 크기 초기화
         SetInfo(0, 30, Color.black);
         bgColorImage.color = Color.white;
         bookCoverColor.color = Color.white;
@@ -56,16 +63,41 @@ public class NK_BookCover : MonoBehaviour
         }
     }
 
+    public void SetBookCoverFont(string fontStyle, string fontColor, string fontSize, string fontPositionX, string fontPositionY)
+    {
+        // 색깔 적용
+        Color colorInfo;
+        ColorUtility.TryParseHtmlString("#" + fontColor, out colorInfo);
+        Int32.TryParse(fontSize, out int size);
+        Int32.TryParse(fontStyle, out int dropboxNum);
+        // 색, 폰트, 크기 초기화
+        SetInfo(dropboxNum, size, colorInfo);
+        inputField.GetComponent<RectTransform>().anchoredPosition = new Vector2(float.Parse(fontPositionX), float.Parse(fontPositionY));
+        // 글씨색
+        inputField.textComponent.color = colorInfo;
+    }
+
+    public void SetBookCover(string coverColor, string sticker, string stickerPositionX, string stickerPositionY)
+    {
+        // 색깔 적용
+        Color colorInfo;
+        ColorUtility.TryParseHtmlString("#" + coverColor, out colorInfo);
+        bgColorImage.color = colorInfo;
+        bookCoverColor.color = colorInfo;
+
+        // 스티커 생성
+        InstantiateObj(sticker, stickerPositionX, stickerPositionY);
+    }
+
     private void Start()
     {
+        txtDropdown.onValueChanged.AddListener(ChangeTextFont);
+        InputtxtSize.onValueChanged.AddListener(ChangeFontSize);
+
         SH_InputField inputText = inputField.GetComponent<SH_InputField>();
 
         SH_EditorManager.Instance.active_InputField = inputText;
-        SetInfo(0, 30, Color.black);
         inputText.SetInfo(txtDropdown.value, int.Parse(InputtxtSize.text), txtcolorImage.color);
-
-        txtDropdown.onValueChanged.AddListener(ChangeTextFont);
-        InputtxtSize.onValueChanged.AddListener(ChangeFontSize);
     }
 
     public void SetInfo(int dropdown, int inputTextSize, Color txtColor)
@@ -92,7 +124,6 @@ public class NK_BookCover : MonoBehaviour
     public void ChangeFontSize(string size)
     {
         SH_EditorManager.Instance.active_InputField.SetFontSize(int.Parse(size));
-        taleInfo.fontSize = size;
     }
 
     public void ChangeTextColor()
@@ -103,13 +134,11 @@ public class NK_BookCover : MonoBehaviour
         ColorUtility.TryParseHtmlString(hexColor[btnNum], out color);
         txtcolorImage.color = color;
         SH_EditorManager.Instance.active_InputField.SetFontColor(color);
-        taleInfo.fontColor = color.ToString();
     }
 
     void ChangeTextFont(int index)
     {
         SH_EditorManager.Instance.active_InputField.SetFontType(index);
-        taleInfo.fontStyle = index.ToString();
     }
 
     public GameObject palette;
@@ -124,7 +153,7 @@ public class NK_BookCover : MonoBehaviour
             palette.SetActive(true);
         }
     }
-    
+
     public GameObject bgPalette;
     public void BGPaletteOnOff()
     {
@@ -138,32 +167,63 @@ public class NK_BookCover : MonoBehaviour
         }
     }
 
+    GameObject createObj;
     public void InstantiateObj()
     {
         // 스티커 선택하면
         GameObject clickBtn = EventSystem.current.currentSelectedGameObject;
         // 스티커에서 버튼 기능을 뺀 같은 객체가 책 위에 생성됨
-        GameObject createObj = Instantiate(clickBtn);
+        createObj = Instantiate(clickBtn);
         createObj.transform.SetParent(bookCover);
         createObj.GetComponent<Button>().enabled = false;
         createObj.GetComponent<RectTransform>().localPosition = Vector3.zero;
         //createObj.GetComponent <RectTransform>().localScale = new Vector3(1, 1, 1);
         createObj.GetComponent<RectTransform>().sizeDelta = new Vector2(120, 120);
-        taleInfo.sticker = createObj.ToString();
-        taleInfo.stickerPositionX = createObj.GetComponent<RectTransform>().localPosition.x.ToString();
-        taleInfo.stickerPositionY = createObj.GetComponent<RectTransform>().localPosition.y.ToString();
     }
 
+    public void InstantiateObj(string btnName, string x, string y)
+    {
+        btnName = btnName.Replace("(Clone) (UnityEngine.GameObject)", "");
+        print(btnName);
+        // 스티커 선택하면
+        GameObject clickBtn = GameObject.Find(btnName);
+        // 스티커에서 버튼 기능을 뺀 같은 객체가 책 위에 생성됨
+        createObj = Instantiate(clickBtn);
+        createObj.transform.SetParent(bookCover);
+        createObj.GetComponent<Button>().enabled = false;
+        createObj.GetComponent<RectTransform>().localPosition = new Vector2(float.Parse(x), float.Parse(y));
+        createObj.GetComponent<RectTransform>().sizeDelta = new Vector2(120, 120);
+    }
+
+    Color bgColor;
     public void ChangeBookColor()
     {
         // 팔레트에서 색 선택하면
         string name = EventSystem.current.currentSelectedGameObject.name;
         int btnNum = int.Parse(name.Substring(3));
         // 책 표지 색상 변경
-        Color color;
-        ColorUtility.TryParseHtmlString(hexColor[btnNum], out color);
-        bgColorImage.color = color;
-        bookCoverColor.color = color;
-        taleInfo.coverColor = color.ToString();
+        ColorUtility.TryParseHtmlString(hexColor[btnNum], out bgColor);
+        bgColorImage.color = bgColor;
+        bookCoverColor.color = bgColor;
+    }
+
+    public void SaveTaleInfo()
+    {
+        // 팔레트 끄기
+        palette.SetActive(false);
+        bgPalette.SetActive(false);
+        // 정보 저장
+        TxtInfo txtInfo = new TxtInfo();
+        SH_SceneObj txt = transform.GetComponentInChildren<SH_SceneObj>();
+        SH_InputField txt2 = transform.GetComponentInChildren<SH_InputField>();
+        taleInfo.fontPositionX = txt.gameObject.GetComponent<RectTransform>().anchoredPosition.x.ToString();
+        taleInfo.fontPositionY = txt.gameObject.GetComponent<RectTransform>().anchoredPosition.y.ToString();
+        taleInfo.fontStyle = txt2.info.txtDropdown.ToString();
+        taleInfo.fontSize = txt2.info.txtSize.ToString();
+        taleInfo.fontColor = ColorUtility.ToHtmlStringRGBA(txt2.transform.GetChild(3).GetComponent<Text>().color);
+        taleInfo.sticker = createObj.ToString();
+        taleInfo.stickerPositionX = createObj.GetComponent<RectTransform>().localPosition.x.ToString();
+        taleInfo.stickerPositionY = createObj.GetComponent<RectTransform>().localPosition.y.ToString();
+        taleInfo.coverColor = ColorUtility.ToHtmlStringRGBA(bgColor);
     }
 }
