9b8dbc8c2c6c4e3e09ec68253b2f2271ed530a0a
Kyounj2 <108376415+Kyounj2@users.noreply.github.com>
2022-11-21 23:47:19 +0900

    1121

---
+++ Assets/YJ/Scripts/YJ_AudioManager.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class YJ_AudioManager : MonoBehaviour
+{
+    Camera main;
+
+    AudioSource audioSet;
+
+
+    
+    [SerializeField]
+    [Header("BGM")]
+    public AudioClip plazaBGM;
+    public AudioClip myRoomBGM;
+    public AudioClip createCharacterBGM;
+    public AudioClip createRoomBGM;
+    public AudioClip fairyBGM;
+    public AudioClip t_bookShelfBGM;
+    public AudioClip c_bookShelfBGM; //아직없음
+
+
+    int winner = 0;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        main = Camera.main;
+        audioSet = main.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // ????
+        if (SceneManager.GetActiveScene().name == "PlazaScene" && main == null)
+        {
+            main = Camera.main;
+            audioSet = main.GetComponent<AudioSource>();
+            audioSet.clip = plazaBGM;
+            audioSet.Play();
+        }
+
+        else if (SceneManager.GetActiveScene().name == "TeacherScene(Candy)" && main == null || SceneManager.GetActiveScene().name == "TeacherScene(ClassRoom)" && main == null || SceneManager.GetActiveScene().name == "TeacherScene(Christmas)" && main == null)
+        {
+            main = Camera.main;
+            audioSet = main.GetComponent<AudioSource>();
+            audioSet.clip = createRoomBGM;
+            audioSet.Play();
+        }
+
+        else if (SceneManager.GetActiveScene().name == "MyRoomScene" && main == null)
+        {
+            main = Camera.main;
+            audioSet = main.GetComponent<AudioSource>();
+            audioSet.clip = myRoomBGM;
+            audioSet.Play();
+        }
+
+        else if (SceneManager.GetActiveScene().name == "CreateCharacter" && main == null)
+        {
+            main = Camera.main;
+            audioSet = main.GetComponent<AudioSource>();
+            audioSet.clip = createCharacterBGM;
+            audioSet.Play();
+        }
+
+        else if (SceneManager.GetActiveScene().name == "Fairy_IHate" && main == null)
+        {
+            main = Camera.main;
+            audioSet = main.GetComponent<AudioSource>();
+            audioSet.clip = fairyBGM;
+            audioSet.Play();
+        }
+
+        else if (SceneManager.GetActiveScene().name == "BookShelfScene" && main == null || SceneManager.GetActiveScene().name == "EditorChildren" && main == null)
+        {
+            main = Camera.main;
+            audioSet = main.GetComponent<AudioSource>();
+            audioSet.clip = t_bookShelfBGM;
+            audioSet.Play();
+        }
+
+        else if (SceneManager.GetActiveScene().name == "EditorScene" && main == null)
+        {
+            return;
+        }
+    }
+}
