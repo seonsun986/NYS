16b01d728fc2538b1904d9d02566e414e0f3de32
kimnakyung <35446851+dd6679@users.noreply.github.com>
2022-11-27 17:25:15 +0900

    update handdown

--- Assets/NK/Scripts/NK_HandControl.cs
+++ Assets/NK/Scripts/NK_HandControl.cs
@@ -12,6 +12,17 @@ public class NK_HandControl : MonoBehaviour
     private void Start()
     {
         handImage.sprite = handdownSprite;
+
+        for (int i = 0; i < GameManager.Instance.children.Count; i++)
+        {
+            if (GameManager.Instance.children[i].Owner.NickName == transform.GetComponentInChildren<Text>().text)
+            {
+                if (GameManager.Instance.children[i].GetComponent<NK_PlayerMove>().state == NK_PlayerMove.State.HandUp)
+                {
+                    handImage.sprite = handupSprite;
+                }
+            }
+        }
     }
 
     private void Update()
