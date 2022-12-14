f2569e914ea2519a786b69e69b60f92df9da97ac
kimnakyung <35446851+dd6679@users.noreply.github.com>
2022-12-14 16:25:24 +0900

    update sound

--- Assets/NK/Scripts/NK_BookUI.cs
+++ Assets/NK/Scripts/NK_BookUI.cs
@@ -4,8 +4,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
-using System.Reflection;
-using UnityEditor.Tilemaps;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.Networking;
